8b638a4821b0868e88dc58ce2ffbf8817004a580
Priscilla Marinho <priscillamarinho2005@gmail.com>
2026-07-14 11:45:56 -0300

    refactor: improved responsiveness + About content + animations

--- src/modules/marketing/pages/LandingPage.tsx
+++ src/modules/marketing/pages/LandingPage.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from '../components/HeroSection'
+import { AboutSection } from '../components/AboutSection'
 
 export function LandingPage() {
   return (
     <main>
       <HeroSection />
+      <AboutSection />
     </main>
   )
 }
